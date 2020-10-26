91f1ab8781ce2efe75263446a5f927c46d3c36aa
Schaap <john.schaap@kadaster.nl>
2020-10-26 15:10:40 +0100

    run generated scripts

--- src/test/java/templateTests/InspireTemplateTest.java
+++ src/test/java/templateTests/InspireTemplateTest.java
@@ -2,19 +2,22 @@ package templateTests;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import filestructure.Templatedir;
 import metadata.DatasetList;
 import org.apache.commons.io.FileUtils;
 
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import static org.apache.commons.io.FileUtils.listFiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -26,14 +29,16 @@ import net.masterthought.cucumber.ReportBuilder;
 import org.junit.jupiter.api.io.TempDir;
 
 class TestAllTemplates {
-    @TempDir static  File defdir;
-    @TempDir static File gentestdir;
+    @TempDir
+    static File defdir;
+    @TempDir
+    static File gentestdir;
 
     @Test
     @Order(1)
     void testTemplateParallel() {
         final Results results = Runner.parallel(getClass(), 1, "target/surefire-reports");
-        generateReport(results.getReportDir());
+
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
@@ -42,26 +47,41 @@ class TestAllTemplates {
     void Createtests() throws IOException {
         loadtestdata(defdir);
         Templatedir templatepath = new Templatedir();
-        File temp = new File (".");
-        System.out.println("templatedir:"+ temp.getAbsolutePath());
+
+        File temp = new File(new File(java.lang.System.getProperty("user.dir")) + File.separator + "src/test/java/templateTests");
+        System.out.println("templatedir:" + temp.getAbsolutePath());
         assertTrue(temp.exists());
         templatepath.builder(temp, ".feature");
         DatasetList.INSTANCE.getInstance().createdirstructure(gentestdir, templatepath);
-        System.out.println("templatedir:"+ gentestdir.getAbsolutePath());
+        System.out.println("gentestdir:" + gentestdir.getAbsolutePath());
         assertTrue(gentestdir.exists());
+        List<String> featurepaths = new ArrayList<>();
+        Iterator<File> matchesIterator = FileUtils.iterateFiles(
+                gentestdir, new WildcardFileFilter("*.feature"), TrueFileFilter.TRUE);
+        while (matchesIterator.hasNext()) {
+            File someFile = matchesIterator.next();
+            String path = someFile.getAbsolutePath();
+            System.out.println("test:" + path);
+            featurepaths.add(path);
+        }
 
+        List<String> tags = List.of("~@ignore");
+        final Results results = Runner.parallel(tags, featurepaths, 4, "target/surefire-reports");
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        generateReport("target/surefire-reports");
     }
 
+
     private void loadtestdata(File defdir) throws IOException {
-        String root = java.lang.System.getenv("MAVEN_PROJECTBASEDIR");
+
         URL url = new URL("https://raw.githubusercontent.com/JLSchaap/nl-ngr-validation/gh-pages/T02_Datasets/datasets.csv");
-        File file = new File(defdir+  File.separator + "datasets.csv");
+        File file = new File(defdir + File.separator + "datasets.csv");
         FileUtils.copyURLToFile(url, file);
         System.out.println(file.getAbsolutePath());
-         assertTrue(file.exists());
+        assertTrue(file.exists());
         DatasetList.INSTANCE.getInstance().loadDataset(file.getAbsolutePath(), true);
         URL url2 = new URL("https://raw.githubusercontent.com/JLSchaap/nl-ngr-validation/gh-pages/T02_Services/services.csv");
-        File servicefile= new File(defdir + File.separator + "services.csv");
+        File servicefile = new File(defdir + File.separator + "services.csv");
         FileUtils.copyURLToFile(url2, servicefile);
         System.out.println(servicefile.getAbsolutePath());
         DatasetList.INSTANCE.getInstance().loadService(servicefile.getAbsolutePath());
@@ -69,7 +89,7 @@ class TestAllTemplates {
 
 
     private static void generateReport(final String karateOutputPath) {
-        final Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" },
+        final Collection<File> jsonFiles = listFiles(new File(karateOutputPath), new String[]{"json"},
                 true);
         final List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
